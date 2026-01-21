db7a57e027cb34d9a6e198c310bb1a077fbc458f
zoebocbig <zoebocquet26@gmail.com>
2026-01-21 15:58:13 +0100

    test de react

--- kahoot/frontend/src/App.jsx
+++ kahoot/frontend/src/App.jsx
@@ -3,6 +3,8 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+import Page1 from './pages/page1'
+
 function App() {
   const [count, setCount] = useState(0)
 
---
+++ kahoot/frontend/src/pages/page1.jsx
@@ -0,0 +1,13 @@
+// src/pages/page1.jsx
+import React from 'react';
+
+const Page1 = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Hello from Page1!</h1>
+      <p>Ce composant est dans le dossier pages.</p>
+    </div>
+  );
+};
+
+export default Page1;
