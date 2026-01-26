cf7e4772a0906b78e0d21aae7dbdfb71956e8ca2
zoebocbig <zoebocquet26@gmail.com>
2026-01-26 12:07:28 +0100

    probleme

--- kahoot/backend/app.py
+++ kahoot/backend/app.py
@@ -31,4 +31,4 @@ def add():
     return "Question ajoutée"
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8080)
+    app.run(host="0.0.0.0", port=5000)
--- kahoot/frontend/src/js/home.js
+++ kahoot/frontend/src/js/home.js
@@ -1,4 +1,4 @@
-fetch("http://backend:8080/api/home")
+fetch("http://backend:5000/api/home")
   .then(res => res.json())
   .then(data => {
     console.log(data);
