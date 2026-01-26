cbf1938f8f3bda351d23e52bfe3882eff406f228
zoebocbig <zoebocquet26@gmail.com>
2026-01-26 10:09:10 +0100

    data base

--- kahoot/backend/app.py
+++ kahoot/backend/app.py
@@ -2,10 +2,13 @@ from flask import Flask, jsonify
 import os
 import requests
 from dotenv import load_dotenv
+from database import init_db, get_questions, add_question
+
 
 load_dotenv()
 
 app = Flask(__name__)
+init_db()
 
 API_KEY = os.getenv("API_KEY")
 
@@ -25,6 +28,15 @@ def kahoot():
 
     response = requests.get(url, headers=headers)
     return response.json()
+
+@app.route("/api/questions")
+def questions():
+    return jsonify(get_questions())
+
+@app.route("/api/add")
+def add():
+    add_question("Capitale de la France ?", "Paris")
+    return "Question ajoutée"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
